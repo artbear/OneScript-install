5972de8c6fa8fba047284afb14eb5b1f387ad6ba
Artur Ayukhanov <aartbear@gmail.com>
2016-08-12 13:25:50 +0300

    Всегда качаю ночную сборку Скачиваю всегда exe-файл вместо скачивания msi

--- install-as-admin.cmd
+++ install-as-admin.cmd
@@ -2,8 +2,11 @@
 
 @choco install curl -y
 
-@curl -o %temp%\oscript-setup.exe http://oscript.io/downloads/latest/msi
-@rem @curl -o %temp%\oscript-setup.exe http://oscript.io/downloads/night-build/msi
+@rem @curl -o %temp%\oscript-setup.exe http://oscript.io/downloads/latest/exe
+@rem 
+@curl -o %temp%\oscript-setup.exe http://oscript.io/downloads/night-build/exe
+
+@%temp%\oscript-setup.exe /silent /log="%temp%\oscript-setup.log" /saveinf="%temp%\oscript-setup-settings.txt"
 
 @%temp%\oscript-setup.exe /silent /log="%temp%\oscript-setup.log"  /saveinf="%temp%\oscript-setup-settings.ini"
 
