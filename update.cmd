6a960db0af7e386b8695e489e7ab28c4e1462593
Artur Ayukhanov <aartbear@gmail.com>
2016-12-28 17:19:32 +0300

    Убрал вывод длинной портянки установки 1скрипт

--- update.cmd
+++ update.cmd
@@ -7,5 +7,5 @@
 @%temp%\oscript-setup.exe /silent /log="%temp%\oscript-setup.log"
 @rem @%temp%\oscript-setup.exe /silent /log="%temp%\oscript-setup.log" /loadinf="%temp%\oscript-setup-settings.ini"
 
-@type %temp%\oscript-setup.log
-@oscript
+@rem @type %temp%\oscript-setup.log
+@oscript -version
