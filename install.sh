f6cea548b6bbab947d89014c60f628d7d159babf
Artur Ayukhanov <aartbear@gmail.com>
2016-12-28 17:19:05 +0300

    Уточнения по установке под linux

--- install.sh
+++ install.sh
@@ -2,18 +2,21 @@
 
 # simple installer for Ubuntu https://github.com/EvilBeaver/OneScript
 
-echo 'Download & install latest release'
+echo 'Download & install night-build release'
 
 wget http://oscript.io/downloads/night-build/deb
+
+sudo apt-get install mono-complete mono-devel
+
 sudo dpkg -i deb
 
 rm deb
 
-sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+# sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 
-sudo su -c 'echo "deb http://download.mono-project.com/repo/debian wheezy main" > /etc/apt/sources.list.d/mono-xamarin.list'
+# sudo su -c 'echo "deb http://download.mono-project.com/repo/debian wheezy main" > /etc/apt/sources.list.d/mono-xamarin.list'
 
-sudo apt-get update
+#sudo apt-get update
 sudo apt-get install -f
 
 echo "Installation complete"
